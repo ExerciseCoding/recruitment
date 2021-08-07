cc645cb6c17ff407529fc42ad39ba1463f33b883
SunshineDay <924345620@qq.com>
2021-08-07 10:41:00 +0800

    从csv中导入候选人

--- interview/management/commands/import_candidates.py
+++ interview/management/commands/import_candidates.py
@@ -3,7 +3,7 @@
 # python manage.py import_candidates --path file.csv
 import csv
 from django.core.management import BaseCommand
-from interview.model import Candiate
+from interview.models import Candidate
 
 class Command(BaseCommand):
     """ 将CSV文件中的候选人信息导入到数据库中
@@ -23,10 +23,21 @@ class Command(BaseCommand):
         根据命令行的字符串解析命令行参数
         Args:
             args:
-            kwargs:
+            kwargs: 命令行参数
         """
         path = kwargs['path']
+        # with open(path , 'rt', encoding='gbk') 已gbk的字符集读取
         with open(path,'rt') as f:
+            # csv.reader(f, dialect='excel',delimiter=';') 解析excel格式以;为分隔符
             reader = csv.reader(f, dialect='excel') #执行解析excel格式
-            for fow in reader:
-                print(row[0])
+            for row in reader:
+                candite = Candidate.objects.create(
+                    username = row[0],
+                    city = row[1],
+                    phone = row[2],
+                    bachelor_school = row[3],
+                    major = row[4],
+                    degree = row[5],
+                    test_score_of_general_ability = row[6],
+                    paper_score = row[7]
+                )
