df0685a6ab2082a7ea23411d9a93871a88018867
SunshineDay <924345620@qq.com>
2021-08-07 11:06:16 +0800

    添加注解

--- jobs/views.py
+++ jobs/views.py
@@ -8,6 +8,13 @@ from django.http import Http404
 
 # Create your views here.
 def joblist(request):
+    """获取职位列表
+    查询所以得职位并返回给前端渲染
+    Args:
+        request: 获取职位列表的请求信息
+    Returns:
+        返回列表并跳转到列表页面    
+    """
     job_list = Job.objects.order_by('job_type')
     template = loader.get_template('joblist.html')
     context = {'job_list': job_list}
@@ -18,6 +25,16 @@ def joblist(request):
 
 
 def detail(request, job_id):
+    """获取职位详情
+    根据职位id查询职位详情
+    Args:
+        request: 获取职位详情的request
+        job_id: 职位id
+    Returns:
+        返回职位详情的数据和要跳转的页面
+    Raises:
+        DoesNotExist: An error occurred when not found job        
+    """
     try:
         job = Job.objects.get(pk=job_id)
         job.city_name = Cities[job.job_city][1]
